e1ebe2535ca4e04a29f44c8458ecf743584a1ef6
Alexey Sachkov <alexey.sachkov@intel.com>
2025-03-26 04:25:27 -0700

    WIP2

--- llvm/cmake/modules/AddSecurityFlags.cmake
+++ llvm/cmake/modules/AddSecurityFlags.cmake
@@ -1,21 +1,14 @@
-macro(add_compile_option_ext flag name)
-  cmake_parse_arguments(ARG "" "" "" ${ARGN}) 
-  set(CHECK_STRING "${flag}")
-  if (MSVC)
-    set(CHECK_STRING "/WX ${CHECK_STRING}")
-  else()
-    set(CHECK_STRING "-Werror ${CHECK_STRING}")
-  endif()
+macro(add_compile_option_ext flag target)
+  string(MAKE_C_IDENTIFIER flag flag_internal)
+  check_c_compiler_flag(flag "C_SUPPORTS_${flag_internal}")
+  check_cxx_compiler_flag(flag "CXX_SUPPORTS_${flag_internal}")
 
-  check_c_compiler_flag("${CHECK_STRING}" "C_SUPPORTS_${name}")
-  check_cxx_compiler_flag("${CHECK_STRING}" "CXX_SUPPORTS_${name}")
-  if (C_SUPPORTS_${name} AND CXX_SUPPORTS_${name})
-    message(STATUS "Building with ${flag}")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}")
-    set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${flag}")
+  message(CHECK_START "Looking if C and CXX compilers support ${flag}")
+  if (C_SUPPORTS_${flag_internal} AND CXX_SUPPORTS_${flag_internal})
+    message(CHECK_PASS "both compilers support, it will be used")
+    target_compile_options(target PRIVATE ${flag})
   else()
-    message(WARNING "${flag} is not supported.")
+    message(CHECK_FAIL "one of compilers doesn't support it and it won't be used!")
   endif()
 endmacro()
 
@@ -30,6 +23,17 @@ macro(add_link_option_ext flag name)
     message(WARNING "${flag} is not supported.")
   endif()
 endmacro()
+
+function(apply_common_extra_security_flags target)
+  if (EXTRA_SECURITY_FLAGS STREQUAL "none")
+  # No actions.
+  elseif (EXTRA_SECURITY_FLAGS STREQUAL "default")
+    append_common_extra_security_flags()
+  elseif (EXTRA_SECURITY_FLAGS STREQUAL "sanitize")
+  add_compile_option_ext("-Wformat" target)
+  add_compile_option_ext("-Wformat-security" target)
+  add_compile_option_ext("-Werror=format-security" target)
+endfunction()
 
 function(append_common_extra_security_flags)
   if( LLVM_ON_UNIX )
@@ -72,32 +76,32 @@ function(append_common_extra_security_flags)
   endif()
 endfunction()
 
-if ( EXTRA_SECURITY_FLAGS )
-    if (EXTRA_SECURITY_FLAGS STREQUAL "none")
-    # No actions.
-    elseif (EXTRA_SECURITY_FLAGS STREQUAL "default")
-      append_common_extra_security_flags()
-    elseif (EXTRA_SECURITY_FLAGS STREQUAL "sanitize")
-      append_common_extra_security_flags()
-      if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-        add_compile_option_ext("-fsanitize=cfi" FSANITIZE_CFI)
-        add_link_option_ext("-fsanitize=cfi" FSANITIZE_CFI_LINK
-          CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS
-          CMAKE_SHARED_LINKER_FLAGS)
-        # Recommended option although linking a DSO with SafeStack is not currently supported by compiler.
-        #add_compile_option_ext("-fsanitize=safe-stack" FSANITIZE_SAFESTACK)
-        #add_link_option_ext("-fsanitize=safe-stack" FSANITIZE_SAFESTACK_LINK
-        #  CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS
-        #  CMAKE_SHARED_LINKER_FLAGS)
-      else()
-        add_compile_option_ext("-fcf-protection=full -mcet" FCF_PROTECTION)
-        # need to align compile and link option set, link now is set unconditionally
-        add_link_option_ext("-fcf-protection=full -mcet" FCF_PROTECTION_LINK
-          CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS
-          CMAKE_SHARED_LINKER_FLAGS)
-      endif()
-    else()
-      message(FATAL_ERROR "Unsupported value of EXTRA_SECURITY_FLAGS: ${EXTRA_SECURITY_FLAGS}")
-    endif()
-endif()
+# if ( EXTRA_SECURITY_FLAGS )
+#     if (EXTRA_SECURITY_FLAGS STREQUAL "none")
+#     # No actions.
+#     elseif (EXTRA_SECURITY_FLAGS STREQUAL "default")
+#       append_common_extra_security_flags()
+#     elseif (EXTRA_SECURITY_FLAGS STREQUAL "sanitize")
+#       append_common_extra_security_flags()
+#       if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+#         add_compile_option_ext("-fsanitize=cfi" FSANITIZE_CFI)
+#         add_link_option_ext("-fsanitize=cfi" FSANITIZE_CFI_LINK
+#           CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS
+#           CMAKE_SHARED_LINKER_FLAGS)
+#         # Recommended option although linking a DSO with SafeStack is not currently supported by compiler.
+#         #add_compile_option_ext("-fsanitize=safe-stack" FSANITIZE_SAFESTACK)
+#         #add_link_option_ext("-fsanitize=safe-stack" FSANITIZE_SAFESTACK_LINK
+#         #  CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS
+#         #  CMAKE_SHARED_LINKER_FLAGS)
+#       else()
+#         add_compile_option_ext("-fcf-protection=full -mcet" FCF_PROTECTION)
+#         # need to align compile and link option set, link now is set unconditionally
+#         add_link_option_ext("-fcf-protection=full -mcet" FCF_PROTECTION_LINK
+#           CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS
+#           CMAKE_SHARED_LINKER_FLAGS)
+#       endif()
+#     else()
+#       message(FATAL_ERROR "Unsupported value of EXTRA_SECURITY_FLAGS: ${EXTRA_SECURITY_FLAGS}")
+#     endif()
+# endif()
 
