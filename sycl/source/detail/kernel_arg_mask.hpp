9e96881ce241cbd55fc0aeba60c5bd39c6584078
Alexey Sachkov <alexey.sachkov@intel.com>
2025-06-30 16:05:52 +0200

    WIP

--- sycl/source/detail/kernel_arg_mask.hpp
+++ sycl/source/detail/kernel_arg_mask.hpp
@@ -13,21 +13,41 @@
 namespace sycl {
 inline namespace _V1 {
 namespace detail {
-using KernelArgMask = std::vector<bool>;
-inline KernelArgMask createKernelArgMask(const ByteArray &Bytes) {
+
+struct KernelArgMask {
+  std::vector<bool> Mask;
+  std::vector<kernel_param_desc_t> Args;
+};
+
+inline KernelArgMask createKernelArgMask(const ByteArray &MaskBytes, const ByteArray &ArgsBytes) {
   const int NBytesForSize = 8;
   const int NBitsInElement = 8;
   std::uint64_t SizeInBits = 0;
 
   KernelArgMask Result;
   for (int I = 0; I < NBytesForSize; ++I)
-    SizeInBits |= static_cast<std::uint64_t>(Bytes[I]) << I * NBitsInElement;
+    SizeInBits |= static_cast<std::uint64_t>(MaskBytes[I]) << I * NBitsInElement;
 
-  Result.reserve(SizeInBits);
+  Result.Mask.reserve(SizeInBits);
   for (std::uint64_t I = 0; I < SizeInBits; ++I) {
-    std::uint8_t Byte = Bytes[NBytesForSize + (I / NBitsInElement)];
+    std::uint8_t Byte = MaskBytes[NBytesForSize + (I / NBitsInElement)];
     Result.push_back(Byte & (1 << (I % NBitsInElement)));
   }
+
+  for (int I = 0; I < NBytesForSize; ++I)
+    SizeInBits |= static_cast<std::uint64_t>(ArgsBytes[I]) << I * NBitsInElement;
+  // We have a set of uint32_t triplets where every triplet describes a single
+  // argument
+  const NumArgs = SizeInBits / /* bits in byte */ 8 / sizeof(uint32_t) / 3;
+  Result.Args.reserve(NumArgs);
+
+  for (int I = 0; i < NumArgs; ++I) {
+    auto Triplet = ArgsBytes.
+  }
+
+  
+
+
   return Result;
 }
 } // namespace detail
